e101c8b5d947e44a9444744399c31d7330652678
Aleksandra Desmurs-Linczewska <desmurs.linczewska@gmail.com>
2020-10-02 11:29:21 +0200

    added request permissons functions plus modal if the location is null

--- App.js
+++ App.js
@@ -1,11 +1,109 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import RNLocation from 'react-native-location';
+import {StyleSheet, View, Text, Modal, TouchableHighlight} from 'react-native';
 import Mountain from './background-components/mountain';
 import Husky from './husky';
+import {fullRelativeWidth, skyColor} from './assets/style_bits';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(() => {
+    RNLocation.configure({
+      distanceFilter: 500, // Meters
+      desiredAccuracy: {
+        ios: 'best',
+        android: 'balancedPowerAccuracy',
+      },
+      // Android only
+      androidProvider: 'auto',
+      interval: 5000, // Milliseconds
+      fastestInterval: 10000, // Milliseconds
+      maxWaitTime: 5000, // Milliseconds
+      // iOS Only
+      activityType: 'other',
+      allowsBackgroundLocationUpdates: false,
+      headingFilter: 1, // Degrees
+      headingOrientation: 'portrait',
+      pausesLocationUpdatesAutomatically: false,
+      showsBackgroundLocationIndicator: false,
+    });
+  }, []);
+
+  useEffect(() => {
+    RNLocation.checkPermission({
+      ios: 'whenInUse', // or 'always'
+      android: {
+        detail: 'coarse', // or 'fine'
+      },
+    }).then((currentPermission) => {
+      if (currentPermission === false) {
+        requestPermission();
+      } else {
+        getLatestLocation();
+      }
+    });
+  }, []);
+
+  const requestPermission = () => {
+    RNLocation.requestPermission({
+      ios: 'whenInUse',
+      android: {
+        detail: 'coarse',
+        rationale: {
+          title: 'We need to access your location',
+          message: 'We use your location to show you the weather in your area',
+          buttonPositive: 'OK',
+          buttonNegative: 'Cancel',
+        },
+      },
+    }).then(getLatestLocation());
+  };
+
+  const getLatestLocation = () => {
+    RNLocation.getLatestLocation({timeout: 1000}).then((latestLocation) => {
+      console.log('what is the lcoation now?', latestLocation);
+      if (latestLocation === null) {
+        setModalVisible(true);
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}>
+        <View style={styles.modalView}>
+          <Text style={[styles.modalText, styles.modalHeader]}>
+            Hello There!
+          </Text>
+          <Text style={styles.modalText}>
+            We were unable to aquire your current location. You will see a
+            weather forecast for the city of Sopot, Poland. Thank you!
+          </Text>
+          <View style={styles.modalButtons}>
+            <TouchableHighlight
+              style={{...styles.openButton, backgroundColor: '#A3ADBB'}}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}>
+              <Text style={styles.textStyle}>That's cool</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={{...styles.openButton, backgroundColor: skyColor}}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}>
+              <Text style={styles.textStyle}>Change my settings</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
       <Mountain />
       <Husky />
     </View>
@@ -18,5 +116,46 @@ const styles = StyleSheet.create({
     backgroundColor: '#4F8EDB',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  modalButtons: {
+    display: 'flex',
+    width: fullRelativeWidth,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  modalHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
   },
 });
--- assets/style_bits.js
+++ assets/style_bits.js
@@ -7,4 +7,5 @@ export const fullRelativeHeight = Dimensions.get('window').width * 1.5;
 export const groundColor = '#dbe8f7';
 export const huskyBodyColor = '#444';
 export const tailColor = '#2c2f36';
+export const skyColor = '#4F8EDB';
 export const animationTimingFunction = Easing.bezier(0.645, 0.045, 0.355, 1);
